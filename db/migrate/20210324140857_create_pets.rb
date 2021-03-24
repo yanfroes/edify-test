7056c3e901261f3d574f213389c39269f693f14a
Yan <yanfroes@gmail.com>
2021-03-24 11:10:58 -0300

    create/migrate users and pets

---
+++ db/migrate/20210324140857_create_pets.rb
@@ -0,0 +1,10 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
